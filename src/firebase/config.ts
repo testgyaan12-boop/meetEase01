a2e170b62480a5d929583bcd78fa80a568271a78
M Khan <mkhan996786@gmail.com>
2026-05-05 13:40:20 +0000

    Final submission error: FirebaseError: Firebase Storage: No default buck

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:888714310704:web:8471164103636feff429f5",
   "apiKey": "AIzaSyB69Wq51ADelIl8qH7ZpoIT4YOQYg-NZd8",
   "authDomain": "studio-9399442289-73214.firebaseapp.com",
+  "storageBucket": "studio-9399442289-73214.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "888714310704"
 };
